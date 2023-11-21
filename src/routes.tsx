d7bb00d461fd09053fd92f5e45247f672df1fabe
Victor Williams <vrwilliams719@gmail.com>
2023-11-21 18:36:24 -0500

    Adding router

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+import { Routes as Router, Route, BrowserRouter } from 'react-router-dom';
+import HomePage from "./frontend/pages/homePage";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Router>
+        <Route path='/' element={<HomePage />} />
+      </Router>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
